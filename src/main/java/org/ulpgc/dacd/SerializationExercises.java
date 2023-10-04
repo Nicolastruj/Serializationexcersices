842e5f968158fb77f53687d080f9a3b5a07525fb
Nicolas <nicolastrujillloestevez@gmail.com>
2023-10-04 16:18:31 +0100

    deserializados commit

--- src/main/java/org/ulpgc/dacd/SerializationExercises.java
+++ src/main/java/org/ulpgc/dacd/SerializationExercises.java
@@ -10,7 +10,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class SerializationExercises {
-    static Gson gson1 = new Gson();
+    private static Gson gson1 = new Gson();
+    private static String jsonSession1;
+    private static String jsonSession2;
+    private static String jsonTheater1;
+    private static String jsonTheater2;
+    private static String jsonMovie1;
+    private static String jsonMovie2;
     public static class Exercise1 {
         public static void main(String[] args) throws IOException {
             Movie film1 = new Movie("Interestelar");
@@ -20,38 +26,38 @@ public class SerializationExercises {
             Session Session1 = new Session(film1, theater1);
             Session Session2 = new Session(film2, theater2);
 
-            String jsonSession1 = gson1.toJson(Session1);
+            jsonSession1 = gson1.toJson(Session1);
             System.out.println(jsonSession1);
             File file1 = new File("text1.txt");
             FileWriter writer1=new FileWriter(file1);
             writer1.write(String.valueOf(jsonSession1));
             writer1.close();
 
-            String jsonSession2 = gson1.toJson(Session2);
+            jsonSession2 = gson1.toJson(Session2);
             File file2 = new File("text2.txt");
             FileWriter writer2=new FileWriter(file2);
             writer2.write(String.valueOf(jsonSession2));
             writer2.close();
 
-            String jsonTheater1 = gson1.toJson(theater1);
+            jsonTheater1 = gson1.toJson(theater1);
             File file3 = new File("text3.txt");
             FileWriter writer3=new FileWriter(file3);
             writer3.write(String.valueOf(jsonTheater1));
             writer3.close();
 
-            String jsonTheater2 = gson1.toJson(theater2);
+            jsonTheater2 = gson1.toJson(theater2);
             File file4 = new File("text4.txt");
             FileWriter writer4=new FileWriter(file4);
             writer4.write(String.valueOf(jsonTheater2));
             writer4.close();
 
-            String jsonMovie1 = gson1.toJson(film1);
+            jsonMovie1 = gson1.toJson(film1);
             File file5 = new File("text5.txt");
             FileWriter writer5=new FileWriter(file5);
             writer5.write(String.valueOf(jsonMovie1));
             writer5.close();
 
-            String jsonMovie2 = gson1.toJson(film2);
+            jsonMovie2 = gson1.toJson(film2);
             File file6 = new File("text6.txt");
             FileWriter writer6=new FileWriter(file6);
             writer6.write(String.valueOf(jsonMovie2));
@@ -66,7 +72,12 @@ public class SerializationExercises {
     public static class Exercise2 {
 
         public static void main(String[] args) {
-            // Session p = gson1.fromJson(jsonString, Person.class);
+            Session session1 = gson1.fromJson(jsonSession1, Session.class);
+            Session session2 = gson1.fromJson(jsonSession2, Session.class);
+            Theater theater1 = gson1.fromJson(jsonTheater1, Theater.class);
+            Theater theater2 = gson1.fromJson(jsonTheater2, Theater.class);
+            Movie movie1 = gson1.fromJson(jsonMovie1, Movie.class);
+            Movie movie2 = gson1.fromJson(jsonMovie2, Movie.class);
         }
     }
 
